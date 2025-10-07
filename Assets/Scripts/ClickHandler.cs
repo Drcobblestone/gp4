d729adb7c9c19d4c3aa52e4fd43c1e97b402fcd0
DoomN00b <blastermaster4@hotmail.com>
2025-10-07 15:18:59 +0200

    Code: finished the simple clickable object script, and made it work to trigger an animation.

--- Assets/Scripts/ClickHandler.cs
+++ Assets/Scripts/ClickHandler.cs
@@ -1,17 +1,47 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
 public class ClickHandler : MonoBehaviour
 {
+    [SerializeField] private bool _hasToBeInRange; //We make sure that the object isn't clickable unless the player is close to it.
+
     [SerializeField] private UnityEvent _clicked;
 
+    private BoxCollider2D _collider; //We make sure that we separate the clicks to only this clickable item, using it's Box-collider.
     private MouseInputProvider _mouse;
+    private bool _isInRange; //If the player is in range, then stuff will happen.
 
     private void Awake()
     {
+        _collider = GetComponent<BoxCollider2D>();
         _mouse = FindObjectOfType<MouseInputProvider>();
         _mouse.Clicked += MouseOnClicked;
     }
+
+    private void MouseOnClicked()
+    {
+        if (_collider.bounds.Contains((Vector3)_mouse.WorldPosition) && (_isInRange || !_hasToBeInRange)   ) //If the collider of the clickable object contains the mouse Worldposition and is in range,
+                                                                                                             //*or* if it's not required to be in range, then I...
+            _clicked.Invoke(); //... invoke the click.
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision) //The player enters the clickable zone.
+    {
+        PlayerController player = collision.GetComponent<PlayerController>(); //Finds the PlayerController, aka the Player, and put the value in there.
+        if (player != null) //Then we check if it really is the player...
+        {
+            _isInRange = true; //And say that the Player is in range.
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision) //The player *EXITS* the clickable zone.
+    {
+        PlayerController player = collision.GetComponent<PlayerController>(); //Finds the PlayerController, aka the Player, and put the value in there.
+        if (player != null) //Then we check if it really is the player...
+        {
+            _isInRange = false; //And say that the Player is *NOT* in range.
+        }
+    }
+
 }
