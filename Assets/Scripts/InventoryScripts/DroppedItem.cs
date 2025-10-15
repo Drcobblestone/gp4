1b45cb9421302317d5111037b4f2041eae15ad32
DoomN00b <blastermaster4@hotmail.com>
2025-10-15 13:18:25 +0200

    Items: swap / spawn in the right place, with correct slicing of sprites. Sprite-sorting: adjusted the bookshelf collider, just right.

--- Assets/Scripts/InventoryScripts/DroppedItem.cs
+++ Assets/Scripts/InventoryScripts/DroppedItem.cs
@@ -9,12 +9,14 @@ public class DroppedItem : MonoBehaviour
     [Header("Settings")]
     [SerializeField] bool autoStart; //This controls if the item is initialized automatically when we start the game, or manually by a script.
 
-    [SerializeField] float enabledPickupDelay = 3.0f; //How long it takes after initialization, before the Player can pick up the item.
+    [SerializeField] float enabledPickupDelay = 1.0f; //How long it takes after initialization, before the Player can pick up the item.
 
     [Header("State")]
     public Item item;
     //public bool pickedUp = false;
     public bool canBePickedUp = true;
+
+    public Vector3 _initialPos = Vector3.zero; //We try to make a value for where the item is first located.
 
     private void Awake()
     {
@@ -26,13 +28,18 @@ public class DroppedItem : MonoBehaviour
 
     }
 
+    private void Start()
+    {
+        _initialPos = transform.position; //Save initial position of the item? I hope.
+    }
+
     public void Initialize(Item item) //When we initialize the dropped item...
     {
         //this.item = item;
         //Drop();
         //GameObject droppedItem = Instantiate(item.prefab, transform); //...we instantiate the dropped item and...
         //droppedItem.transform.SetLocalPositionAndRotation(Vector2.zero, Quaternion.identity); //...set its position and rotation in the place.
-        
+
     }
     public void Drop()
     {
