f6833115176ffec83ec8878030accef1d43dfaa4
DoomN00b <blastermaster4@hotmail.com>
2025-11-27 10:52:42 +0100

    Confirmed that we can't have main camera in singleton. We will now set it up per-scene instead. Removed camera from singleton.

--- Assets/Scripts/PlayerScripts/FootStepSounds.cs
+++ Assets/Scripts/PlayerScripts/FootStepSounds.cs
@@ -7,8 +7,8 @@ public class FootStepSounds : MonoBehaviour
     [SerializeField] PlayerController playerController;
     [SerializeField] AudioClip footStep1;
     [SerializeField] AudioClip footStep2;
-    //[SerializeField] Camera mainCamera;
-    [SerializeField] GameObject LLSingleton;
+    [SerializeField] Camera mainCamera;
+    
 
 
     //[SerializeField] AudioSource footStepSound;
@@ -18,7 +18,7 @@ public class FootStepSounds : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        GetObject.
+        
     }
 
     // Update is called once per frame
