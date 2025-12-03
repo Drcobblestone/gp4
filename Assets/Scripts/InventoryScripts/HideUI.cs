57abc65951f49e511543ca02a605df2436c19c38
DoomN00b <blastermaster4@hotmail.com>
2025-12-03 19:09:26 +0100

    Tried to make inventory re-enable, but it doesn't work.

--- Assets/Scripts/InventoryScripts/HideUI.cs
+++ Assets/Scripts/InventoryScripts/HideUI.cs
@@ -10,19 +10,37 @@ public class HideUI : MonoBehaviour
     [SerializeField] private InputActionReference visibleUi; //We get the VisibleUi-action from the input action asset - so that we can use it to make canvas invisible.
 
     [Header("(Put MainMenuCanvas here)")]
-    [SerializeField] private GameObject mainMenuCanvas ; //A field where we put our MainMenuCanvas inside, so we can add it to an if-statement, if this canvas should be active.
+    //[SerializeField] private GameObject mainMenuCanvas ; //A field where we put our MainMenuCanvas inside, so we can add it to an if-statement, if this canvas should be active.
+    [Header("(Put the SceneData here.)")]
+    [SerializeField] SceneData sceneData;
 
 
+    /*
     public void ToggleVisibility() //This function controls if the inventory is visible.
     {
         //Add If-statement to make it inactive depending on if it's main menu or not.
         if (mainMenuCanvas == null) //If there's no mainMenuCanvas, then...
         {
             canvasInventory.SetActive(!canvasInventory.activeInHierarchy); //Activate the Inventory-canvas if it's not active in the hierarchy.
+            Logging.Log($"Turned on the Inventory.");
         }
-
     }
-    
+    */
+
+    //New experiment!
+    public void ToggleVisibility() //This function controls if the inventory is visible.
+    {
+        if (sceneData.sceneName != "MainMenu" || sceneData.sceneName != "BookClub" || sceneData == null) //If there's no mainMenuCanvas, then...
+        {
+            canvasInventory.SetActive(!canvasInventory.activeInHierarchy); //Activate the Inventory-canvas if it's not active in the hierarchy.
+            Logging.Log($"Turned on the Inventory.");
+        }
+    }
+
+
+
+
+
     public void OnEnable()
     {
         visibleUi.action.Enable(); //When we enable the Cancel-action...
