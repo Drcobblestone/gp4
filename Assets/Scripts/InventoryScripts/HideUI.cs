2e811a05496fd389719c2515fd55ab415b5dff7a
DoomN00b <blastermaster4@hotmail.com>
2025-12-04 15:26:12 +0100

    CODE: rewriting HideUI to make it work again... Added a new field in scenedata that tells if the scene is meant to have inventory visible.

--- Assets/Scripts/InventoryScripts/HideUI.cs
+++ Assets/Scripts/InventoryScripts/HideUI.cs
@@ -9,25 +9,10 @@ public class HideUI : MonoBehaviour
     [SerializeField] private GameObject canvasInventory; //A field where we put our entire CanvasInventory object inside, so this script can make it invisible.
     [SerializeField] private InputActionReference visibleUi; //We get the VisibleUi-action from the input action asset - so that we can use it to make canvas invisible.
 
-    //[Header("(Put MainMenuCanvas here)")]
-    //[SerializeField] private GameObject mainMenuCanvas ; //A field where we put our MainMenuCanvas inside, so we can add it to an if-statement, if this canvas should be active.
     [Header("(Put the SceneData here.)")]
     [SerializeField] SceneData sceneData;
 
-
     /*
-    public void ToggleVisibility() //This function controls if the inventory is visible.
-    {
- 
-        if (mainMenuCanvas == null) //If there's no mainMenuCanvas, then...
-        {
-            canvasInventory.SetActive(!canvasInventory.activeInHierarchy); //Activate the Inventory-canvas if it's not active in the hierarchy.
-            Logging.Log($"Turned on the Inventory.");
-        }
-    }
-    */
-
-    //New experiment!
     public void ToggleVisibility() //This function controls if the inventory is visible.
     {
         if (sceneData.sceneName != "MainMenu" || sceneData.sceneName != "BookClub") //If the scene isn't called Mainmenu or BookClub, or if there's no Scene-data, then we do this.
@@ -39,17 +24,48 @@ public class HideUI : MonoBehaviour
         {
             Logging.Log($"We didn't turn on the inventory.");
         }
-
     }
+    */
 
-
-
-
-
+    //Let's turn this off temporarily...
+    /*
     public void OnEnable()
     {
         visibleUi.action.Enable(); //When we enable the Cancel-action...
         visibleUi.action.performed += ctx => ToggleVisibility(); //...Then we make the canvas invisible.
         Logging.Log($"We made the inventory invisible.");
     }
+    */
+
+    public void Awake() //This function controls if the inventory is visible.
+    {
+        ToggleVisibility(); //We start the co-routine for controlling inventory visibility.
+        Logging.Log($"We started togglevisibility co-routine.");
+    }
+
+    IEnumerator ToggleVisibility()
+    {
+        Logging.Log($"Toggle-vis running.");
+        if (sceneData.inventoryScene != "Yes") //If the scene isn't called Mainmenu or BookClub, or if there's no Scene-data, then we do this.
+        {
+            canvasInventory.SetActive(!canvasInventory.activeInHierarchy); //Activate the Inventory-canvas if it's not active in the hierarchy.
+            Logging.Log($"Turned on the Inventory.");
+            yield break;
+        }
+        else
+        {
+            Logging.Log($"We didn't turn on the inventory.");
+            yield break;
+        }
+    }
+
+    public void NotVisible()
+    {
+        Logging.Log($"Running NotVisible");
+        visibleUi.action.Enable();
+        visibleUi.action.performed += ctx => ToggleVisibility(); //...Then we make the canvas invisible.
+        Logging.Log($"We made the inventory invisible.");
+
+    }
+
 }
--- Assets/Scripts/SceneData.cs
+++ Assets/Scripts/SceneData.cs
@@ -9,6 +9,8 @@ public class SceneData : ScriptableObject
     public string sceneName = ""; //We can use any name for the scene in the script.
     public int entryIndex = -1; //We set the entry-point index default value to -1, so it won't work in an Array, and we can put an error-message for when we haven't assigned any point to it.
 
+    public string inventoryScene = ""; //We will assign yes or no here, to tell the HideUI-script whether it should hide the inventory.
+
     [Header("The items the scene has when loaded.")] //Tells us where to drop items that are meant to be in the scene.
     public bool[] sceneFlags = new bool[8]; // [X][ ][X][ ][ ][ ][ ][ ][ ][ ]
     public List<ItemToSpawn> itemsToSpawn = new List<ItemToSpawn>();   
