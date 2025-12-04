a42194f5f89e73df6974bc8039cf1108582303e8
DoomN00b <blastermaster4@hotmail.com>
2025-12-04 13:45:34 +0100

    EDITOR: Added the scene data to some of the scenes, for the HideUI script to work. Which is odd, since I could swear I've already done so...

--- Assets/Scripts/InventoryScripts/HideUI.cs
+++ Assets/Scripts/InventoryScripts/HideUI.cs
@@ -9,7 +9,7 @@ public class HideUI : MonoBehaviour
     [SerializeField] private GameObject canvasInventory; //A field where we put our entire CanvasInventory object inside, so this script can make it invisible.
     [SerializeField] private InputActionReference visibleUi; //We get the VisibleUi-action from the input action asset - so that we can use it to make canvas invisible.
 
-    [Header("(Put MainMenuCanvas here)")]
+    //[Header("(Put MainMenuCanvas here)")]
     //[SerializeField] private GameObject mainMenuCanvas ; //A field where we put our MainMenuCanvas inside, so we can add it to an if-statement, if this canvas should be active.
     [Header("(Put the SceneData here.)")]
     [SerializeField] SceneData sceneData;
@@ -18,7 +18,7 @@ public class HideUI : MonoBehaviour
     /*
     public void ToggleVisibility() //This function controls if the inventory is visible.
     {
-        //Add If-statement to make it inactive depending on if it's main menu or not.
+ 
         if (mainMenuCanvas == null) //If there's no mainMenuCanvas, then...
         {
             canvasInventory.SetActive(!canvasInventory.activeInHierarchy); //Activate the Inventory-canvas if it's not active in the hierarchy.
@@ -30,11 +30,16 @@ public class HideUI : MonoBehaviour
     //New experiment!
     public void ToggleVisibility() //This function controls if the inventory is visible.
     {
-        if (sceneData.sceneName != "MainMenu" || sceneData.sceneName != "BookClub" || sceneData == null) //If there's no mainMenuCanvas, then...
+        if (sceneData.sceneName != "MainMenu" || sceneData.sceneName != "BookClub") //If the scene isn't called Mainmenu or BookClub, or if there's no Scene-data, then we do this.
         {
             canvasInventory.SetActive(!canvasInventory.activeInHierarchy); //Activate the Inventory-canvas if it's not active in the hierarchy.
             Logging.Log($"Turned on the Inventory.");
         }
+        else
+        {
+            Logging.Log($"We didn't turn on the inventory.");
+        }
+
     }
 
 
@@ -45,5 +50,6 @@ public class HideUI : MonoBehaviour
     {
         visibleUi.action.Enable(); //When we enable the Cancel-action...
         visibleUi.action.performed += ctx => ToggleVisibility(); //...Then we make the canvas invisible.
+        Logging.Log($"We made the inventory invisible.");
     }
 }
