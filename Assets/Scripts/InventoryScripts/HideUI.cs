1e3ff57431fcfd5883ac7ef607c9c79cbaa9f9ca
DoomN00b <blastermaster4@hotmail.com>
2025-12-08 13:24:30 +0100

    EDITOR: tried some bullshit with adding camera to singleton and then changing it to Screen space instead of overlay - which works in making it not see the inventory in ManMenu et c scenes, but which then makes it end up underneath objects in the scene. But I changed it back, and then I changed the collider on Frog-pickups.

--- Assets/Scripts/InventoryScripts/HideUI.cs
+++ Assets/Scripts/InventoryScripts/HideUI.cs
@@ -12,24 +12,25 @@ public class HideUI : MonoBehaviour
     [Header("(Put the SceneData here.)")]
     [SerializeField] SceneData sceneData;
 
-    
-    
+
+    //This version doesn't work, as it gets stuck in the Singleton, and can never change to being toglable again.
+    /*
     public void OnEnable()
     {
         //Should I try a switch instead? Perhaps those can be reset in-between scenes, unlike this if-statement that can never RE-check itself, it seems.
 
-            /*
-            if (sceneData.inventoryScene == "No")
-            {
-                visibleUi.action.Disable();
-                Logging.Log($"We disabled the inputaction to toggle Inventory-visibility, because it's not supposed to be visible.");
 
-                canvasInventory.SetActive(false); //We make the inventory invisible.
-                Logging.Log($"We turned off the inventory.");
-            }
-            */
-        if (sceneData.inventoryScene == "Yes")
-        //else if (sceneData.inventoryScene == "Yes")
+        if (sceneData.inventoryScene == "No")
+        {
+            visibleUi.action.Disable();
+            Logging.Log($"We disabled the inputaction to toggle Inventory-visibility, because it's not supposed to be visible.");
+
+            canvasInventory.SetActive(false); //We make the inventory invisible.
+            Logging.Log($"We turned off the inventory.");
+        }
+   
+        //if (sceneData.inventoryScene == "Yes")
+        else if (sceneData.inventoryScene == "Yes")
         {
             visibleUi.action.Enable(); //We enable the Visible-action...
             Logging.Log($"Enabled visibleUI action.");
@@ -38,6 +39,23 @@ public class HideUI : MonoBehaviour
             visibleUi.action.performed += ctx => ToggleVisibility(); //...Then we allow for toggling visibility through the InputAction.
             Logging.Log($"We can toggle inventory visibility");
         }
+    }
+    */
+
+    public void Awake()
+    {
+        //Create code that seeks out the camera if we are in a scene marked NOT-inventory and inserts it into the IngameMenu-objects Canvas/Render mode/Render Camera
+    }
+
+
+    public void OnEnable()
+    {
+            visibleUi.action.Enable(); //We enable the Visible-action...
+            Logging.Log($"Enabled visibleUI action.");
+            canvasInventory.SetActive(true); //We make the inventory visible.
+
+            visibleUi.action.performed += ctx => ToggleVisibility(); //...Then we allow for toggling visibility through the InputAction.
+            Logging.Log($"We can toggle inventory visibility");
     }
 
     public void ToggleVisibility() //This function controls if the inventory is visible.
