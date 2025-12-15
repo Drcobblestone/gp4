f4e416a34dd8178fcc5da2735ebd737997f7c4a5
DoomN00b <blastermaster4@hotmail.com>
2025-12-15 13:34:38 +0100

    CODE: Got help from  Tom to fix pause-menu, it was partially a Unity-bug. It's often better to hardcode a specific InputActionReference (like Pause), than doing it otherwise.

--- Assets/Scripts/MainMenu/PauseMenu.cs
+++ Assets/Scripts/MainMenu/PauseMenu.cs
@@ -1,44 +1,84 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.Burst.Intrinsics;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using UnityEngine.InputSystem.Controls;
 using UnityEngine.SceneManagement;
-using UnityEngine.UI;
 
-//This script controls the pause-menu, and let's you activate it with different inputs.
 public class PauseMenu : MonoBehaviour
 {
-    [SerializeField] InputActionReference cancelAction; //We get the cancel-action from the input action asset - we're going to use that to Quit.
+    [SerializeField] private InputActionReference pause;
     [Header("Put the item called PauseMenu here.")]
     [SerializeField] GameObject pauseMenu; //We put the pause-menu here.
     private PlayerController player; //We get the player so we can pause him.
+    private BackgroundMusic music; //We get the music so we can lower the volume.
 
-    protected bool isPaused = false;
+    protected bool isPaused = false; //This helps us keep track of if the game is paused, and the pause-menu is up.
 
-    private void Awake()
+
+
+    // Awake is run immediately after the object is instantiated, and before Start or OnEnable. It's where you set up essential data or assign references for other scripts.
+    void Awake()
     {
-        player = PlayerController.Instance;
+        player = PlayerController.Instance; //We make sure we're running the current instance of PlayerController.
 
-        if (pauseMenu.activeInHierarchy)
-        {
-            pauseMenu.SetActive(false); //We turn off the pause-menu, because it's not ussually meant to be on.
-            Logging.Log($"Made Pause-menu not visible to start with.");
-        }
+        //music = LLSingleton.Instance.music; //We summon the latest instance of the Backgroundmusic script in the singleton; so we can change it's volume later.
+
+        Logging.Log($"We ran awake in Pausemenu."); 
+
     }
 
 
+    private void Start()
+    {
+        pause.action.Enable();
+        pause.action.performed += ctx => TogglePause(ctx);
+        print("started");
+
+        if (pauseMenu.activeInHierarchy) //If the pause-menu is already active in the hierarchy when the scene loads, then...
+        {
+            pauseMenu.SetActive(false); //...We turn off the pause-menu, because it's not ussually meant to be on.
+            Logging.Log($"Made Pause-menu not visible to start with.");
+        }
+        else //otherwise, if it's not on, then...
+        {
+            Logging.Log($"No need to make pause-menu invisible.");
+            return; //...we don't do anything.
+        }
+    }
+
+    
+    public void TogglePause(InputAction.CallbackContext context)
+    {
+        Logging.Log($"Ran Togglepause, but didn't set bool yet.");
+
+        if (!context.performed)
+        {
+            return;
+        }
+        isPaused = !isPaused; //We make it so that when we toggle the cancel-action with a key-press, we invert the setting of the isPaused-bool.
+        
+        pauseMenu.SetActive(isPaused); //Whether the pause-menu is active is now dependent on whether the isPaused-bool is true or false. (if it's true, the pause-menu should be active)
+
+        Time.timeScale = isPaused ? 0 : 1; //Time will go from, fully on (1), when isPaused is false (its standard state), to fully off (0), when isPaused is true.
+
+        //music.musicSource.volume = isPaused ? 0.75f : 0.2f; //The music will go from it's standard 75% output to 20% output depending on the state of isPaused.
+
+        //player.movementLocked = isPaused; //We make it so that the players movementlock bool is set to whatever the isPaused bool is set to.
+
+        Logging.Log($"We have toggled Pause {isPaused}");
+    }
+    
 
     //----Canvas stuff
     public void ResumeGame() //When we resume the game, we will use this function in the Canvas.
     {
-        Time.timeScale = 1;
-        //.Resume();
-        isPaused = false;
-        pauseMenu.SetActive(false);
-        Logging.Log($"Turned off Pause-menu.");
+        isPaused = false; //We make it so that when we toggle the cancel-action with a key-press, we invert the setting of the isPaused-bool.
+
+        pauseMenu.SetActive(false); //Whether the pause-menu is active is now dependent on whether the isPaused-bool is true or false. (if it's true, the pause-menu should be active)
+
+        Time.timeScale = 1.0f;
+
     }
 
     public void BacktoMain() //When we click "Go back to main menu" in the canvas, we will use this.
@@ -48,50 +88,4 @@ public class PauseMenu : MonoBehaviour
 
     //----Canvas end.
 
-
-    void FixedUpdate()
-    {
-        if (!cancelAction.action.triggered) //If the player didn't press Cancel, then we don't do anything.
-        {
-            return;
-        }
-
-        else if (cancelAction.action.triggered) //But if we DID press cancel..!
-        {
-            Time.timeScale = 0;
-            pauseMenu.SetActive(true);
-            //player.Pause();
-            //isPaused = true;
-        }
-        /*
-        else //Otherwise we...
-        {
-            ResumeGame(); //...resume the game again.
-        }
-        */
-    }
-
-
-    /*
-    private void OnEnable()
-    {
-        cancelAction.action.Enable(); //When we enable the Cancel-action...
-        Logging.Log($"We can Cancel");
-        cancelAction.action.performed += ctx => isPaused = true; //...We bring up the Pause menu.
-
-        Logging.Log($"We perform the cancel-action.");
-        //player.Pause(); //We freeze the player.
-    }
-
-
-    private void OnDisable()
-    {
-        Logging.Log($"We Stopped"); //This will only be visible in-editor.
-        cancelAction.action.Disable(); //We turn off the ability to quit the game with the Cancel-action - when we don't need it.
-        Logging.Log($"Turning off Cancel.");
-        player.Resume();
-        Logging.Log($"Unfreezing the player.");
-
-    }
-    */
 }
